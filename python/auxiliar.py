de8319248740d3c327c2f77e7bccf92e9d7ce0a6
Danilo Oliveira <danilo.t.a.oliveira@gmail.com>
2023-04-08 22:14:35 -0300

    adicionado sequencial em python

---
+++ python/auxiliar.py
@@ -0,0 +1,46 @@
+import sys
+import time
+import numpy as np
+
+if len(sys.argv) != 5:
+    print("Use o comando: python auxiliar.py n1 m1 n2 m2")
+    sys.exit()
+
+# Tempo inicial total
+t0 = time.time()
+
+# Recebe os parâmetros
+n1 = int(sys.argv[1])
+m1 = int(sys.argv[2])
+n2 = int(sys.argv[3])
+m2 = int(sys.argv[4])
+# Cria as matrizes M1 e M2
+M1 = np.round(np.random.uniform(low=1.0, high=9.0, size=(n1, m1)), decimals=2)
+M2 = np.round(np.random.uniform(low=1.0, high=9.0, size=(n2, m2)), decimals=2)
+
+# Salva as matrizes em arquivos .txt
+with open('M1.txt', 'w') as f:
+    # Tempo inicial
+    t1 = time.time()
+    f.write(f'{n1} {m1}\n')
+    for i in range(n1):
+        for j in range(m1):
+            f.write(f'c{i+1}{j+1} {M1[i,j]:.0f}\n')
+            t2 = time.time()
+    f.write(f'{t2-t1:.3f}')
+
+with open('M2.txt', 'w') as f:
+    t3 = time.time()
+    f.write(f'{n2} {m2}\n')
+    for i in range(n2):
+        for j in range(m2):
+            f.write(f'c{i+1}{j+1} {M2[i,j]:.0f}\n')
+            t4 = time.time()
+    f.write(f'{t4-t3:.3f}')
+
+
+# Tempo final total
+t5 = time.time()
+
+# Resultado do tempo gasto
+print(f'{t5-t0:.3f}')
---
+++ python/sequencial.py
@@ -0,0 +1,64 @@
+import sys
+import time
+
+# leitura dos argumentos
+if len(sys.argv) != 4:
+    print("Use o comando: python sequencial.py M1.txt M2.txt M3.txt")
+    sys.exit()
+
+arquivo1 = sys.argv[1]
+arquivo2 = sys.argv[2]
+arquivo3 = sys.argv[3]
+
+# leitura da matriz 1
+with open(arquivo1, "r") as f:
+    # leitura do tamanho da matriz
+    m, n = map(int, f.readline().split())
+    # inicialização da matriz
+    M1 = [[0 for j in range(n)] for i in range(m)]
+    # leitura dos elementos
+    i = 0
+    while i < m:
+        j = 0
+        while j < n:
+          line = f.readline().split()
+          # preenchimento da matriz
+          M1[i][j] = int(line[1])
+          j += 1
+        i += 1
+
+# leitura da matriz 2
+with open(arquivo2, "r") as f:
+    # leitura do tamanho da matriz
+    p, q = map(int, f.readline().split())
+    # inicialização da matriz
+    M2 = [[0 for j in range(q)] for i in range(p)]
+    # leitura dos elementos
+    i = 0
+    while i < p:
+        j = 0
+        while j < q:
+          line = f.readline().split()
+          # preenchimento da matriz
+          M2[i][j] = int(line[1])
+          j += 1
+        i += 1
+
+# cálculo da matriz resultante
+t0 = time.time()
+M3 = [[0 for j in range(q)] for i in range(m)]
+for i in range(m):
+    for j in range(q):
+        for k in range(n):
+            M3[i][j] += M1[i][k] * M2[k][j]
+
+# escrita da matriz resultante
+with open(arquivo3, "w") as f:
+    f.write(f"{m} {q}\n")
+    for i in range(m):
+        for j in range(q):
+            f.write(f"c{i+1}{j+1} {M3[i][j]}\n")
+    t1 = time.time()
+    f.write(f'{t1-t0:.3f}')
+
+print(f'{t1-t0:.3f}')
